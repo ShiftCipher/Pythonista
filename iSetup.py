07fe3bf344085bb2b3eebaa2abfccb2af16594d0
codeapps <codeapps@aol.com>
2016-07-06 14:58:53 -0500

    iSetup Improve

--- iSetup.py
+++ iSetup.py
@@ -5,59 +5,21 @@ import os
 import json
 import urllib2
 
-print "Welcome... TO THE WORLD OF TOMORROW"
+print "Welcome to the World of Tomorrow"
 
 name = ''
 email = ''
-options = { 'developer': '', 'android': '', 'ios': '', 'designer': '',
-            'sublime': '', 'zsh': '',
-            'animations': '', 'showhiddenfiles': '', 'autoupdate': '', }
-
-
-# Sudo: Spectacle, ZSH, OSX Settings
 
 # Basic Info
 while name == '':
-  name = raw_input("What's your name?\n").strip()
+  name = raw_input("What's your Name?\n").strip()
 
 while email == '' or '@' not in email:
-  email = raw_input("What's your email?\n").strip()
+  email = raw_input("What's your Email?\n").strip()
 
-
-# Setup Options
-while options['developer'] not in ['y', 'n']:
-  options['developer'] = raw_input("Do you want to install Developer Tools? (%s)  " % '|'.join(['y','n']))
-
-if options['developer'] == 'y':
-  while options['android'] not in ['y', 'n']:
-    options['android'] = raw_input("Do you want to install Android Tools? (%s)  " % '|'.join(['y','n']))
-
-  while options['ios'] not in ['y', 'n']:
-    options['ios'] = raw_input("Do you want to install iOS Tools? (%s)  " % '|'.join(['y','n']))
-
-while options['designer'] not in ['y', 'n']:
-  options['designer'] = raw_input("Do you want to install Designer Tools? (%s)  " % '|'.join(['y','n']))
-
-
-
-while options['sublime'] not in ['y', 'n']:
-  options['sublime'] = raw_input("Do you want to install Sublime Text 3 with Plugins? (%s)  " % '|'.join(['y','n']))
-
-while options['zsh'] not in ['y', 'n']:
-  options['zsh'] = raw_input("Do you want to install Oh My Zsh? (%s)  " % '|'.join(['y','n']))
-
-while options['animations'] not in ['y', 'n']:
-  options['animations'] = raw_input("Do you want to accelerate OSX animations? (%s)  " % '|'.join(['y','n']))
-
-while options['showhiddenfiles'] not in ['y', 'n']:
-  options['showhiddenfiles'] = raw_input("Do you want to show hidden files? (%s)  " % '|'.join(['y','n']))
-
-while options['autoupdate'] not in ['y', 'n']:
-  options['autoupdate'] = raw_input("Do you want to update your computer automatically? (Recommended) (%s)  " % '|'.join(['y','n']))
-
-
-print "Hi %s!" % name
+print "Welcome. Sr. %s!" % name
 print "You'll be asked for your password at a few points in the process"
+# Sudo: Spectacle, ZSH, OSX Settings
 print "*************************************"
 print "Setting up your Mac..."
 print "*************************************"
@@ -88,216 +50,185 @@ if os.system('xcode-select -p') != 0:
 
 # Install Brew & Brew Cask
 print "Installing Brew & Brew Cask"
-os.system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
-os.system('brew tap caskroom/cask')
+os.system('ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
 os.system('brew tap homebrew/services')
+os.system('brew tap homebrew/versions')
+os.system('brew tap caskroom/cask')
 os.system('brew tap caskroom/versions')
 os.system('brew tap caskroom/fonts')
-os.system('brew tap homebrew/versions')
 os.system('brew update && brew upgrade && brew cleanup && brew cask cleanup')
 
+print "Enabling Automatic Brew Updates & Upgrades"
+os.system('brew tap domt4/autoupdate')
+os.system('brew autoupdate --start --upgrade')
 
 # Install Languages
-print "Installing Git+NodeJS+Python+Ruby"
+print "Installing Git NodeJS Python Ruby"
 os.system('brew install git node python python3 ruby')
 os.system('brew link --overwrite git node python python3 ruby')
 os.system('brew install git-flow')
 
-print "Installing Useful Stuff"
-os.system('brew install graphicsmagick curl wget sqlite libpng libxml2 openssl')
+print "Installing Brews"
+os.system('brew install curl wget')
+
+node = '''
+nvm
+node-gyp
+express-generator
+pug
+pug-cli
+gulp-cli
+nodemon
+browser-sync
+bower
+gulp
+postcss-cli
+autoprefixer
+uglify-js
+clean-css
+imagemin
+imagemin-cli
+node-inspector
+node-gyp
+nvm
+pm2
+istanbul
+babel-cli
+testling
+karma-cli
+browserify
+tap-colorize
+caminte-cli
+'''
 
 print "Installing Command Line Tools"
-os.system('npm install -g yo bower gulp grunt grunt-cli node-gyp nvm')
-
+os.system('npm isntall -g "%s"' % node)
 
 # OSX Tweaks & Essentials
 print "Installing Quicklook Helpers"
-os.system('brew cask install qlcolorcode qlmarkdown qlimagesize quicklook-csv quicklook-json webpquicklook suspicious-package epubquicklook qlstephen qlprettypatch betterzipql font-hack')
+os.system('brew cask install qlcolorcode qlmarkdown qlimagesize quicklook-csv quicklook-json webpquicklook')
 
 print "Installing Fonts"
-os.system('brew cask install font-dosis font-droid-sans font-open-sans font-open-sans-condensed font-roboto font-roboto-mono font-roboto-condensed font-roboto-slab font-arial font-arial-black font-consolas-for-powerline font-dejavu-sans font-dejavu-sans-mono-for-powerline font-georgia font-inconsolata font-inconsolata-for-powerline font-lato font-menlo-for-powerline font-meslo-lg font-meslo-lg-for-powerline font-noto-sans font-noto-serif font-source-sans-pro font-source-serif-pro font-verdana font-times-new-roman font-ubuntu font-pt-mono font-pt-sans font-pt-serif font-fira-mono font-fira-mono-for-powerline font-fira-code font-fira-sans font-fontawesome font-source-code-pro font-anka-coder')
+os.system('brew cask install robofont fontforge birdfont skyfonts trufont')
+
+apps = '''
+whatsapp
+blender
+freecad
+kicad
+fritzing
+sketchup
+dropbox
+spectacle
+the-unarchiver
+cleanmymac 
+pdfexpert
+evernote
+odrive
+rescuetime
+google-chrome
+duet
+skype
+syncmate
+1password
+pdfexpert
+cleanmymac
+parallels-desktop
+rescuetime
+slack
+gitter
+airserver
+beamer
+paw
+screens
+screens-connect
+reveal
+soundnode
+epic-games-launcher
+'''
+
+developer = '''
+node
+atom
+gitkraken
+postgres
+pgadmin3
+redis
+mamp
+iterm2
+shuttle
+staruml
+webstorm
+clion
+phpstorm
+appcode
+datagrip
+ngrok
+rightfont
+'''
+
+designer = '''
+adobe-creative-cloud
+adobe-photoshop-cc
+adobe-illustrator-cc
+adobe-indesign-cc
+schnapps
+invisionsync
+iconjar
+sketch-tool
+sketch-toolbox
+framer-studio
+origami
+principle
+'''
 
 print "Installing Essential Apps"
-os.system('brew cask install iterm2 spectacle the-unarchiver')
-os.system('brew cask install google-chrome firefox sourcetree sublime-text3 atom dropbox skype spotify slack google-hangouts vlc macdown')
+os.system('brew cask install "%s"' % apps)
 
+print "Installing iOS Tools"
+os.system('sudo gem install cocoapods')
 
-# Appropriate Software
-if options['developer'] == 'y':
-  print "Installing Developer Tools"
-  os.system('brew cask install sequel-pro cyberduck dockertoolbox ngrok')
+print "Installing Designer Tools"
+os.system('brew cask install "%s"' % designer)
 
-if options['android'] == 'y':
-  print "Installing Android Tools"
-  os.system('brew cask install java')
-  os.system('brew cask install android-studio')
-  os.system('brew install android-platform-tools')
-
-if options['ios'] == 'y':
-  print "Installing iOS Tools"
-  os.system('sudo gem install cocoapods')
-
-if options['designer'] == 'y':
-  print "Installing Designer Tools"
-  os.system('brew cask install invisionsync iconjar skala-preview')
-  #os.system('brew cask install sketch-tool principle framer-studio origami')
-
-
-# Sublime Text 3 Package Control & Binding
-sublime_settings_path = os.path.expanduser("~") + '/Library/Application Support/Sublime Text 3/'
-sublime_user_path = sublime_settings_path + 'Packages/User/'
-
-if not os.path.isfile( sublime_settings_path + 'Installed Packages/Package Control.sublime-package'):
-  print "Installing Package Control"
-  os.system('wget -P ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages https://packagecontrol.io/Package%20Control.sublime-package')
-
-os.system('ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime')
-
-
-# Sublime Text 3 with Recommended Packages
-if options['sublime'] == 'y':
-  print "Customizing Sublime Text"
-
-  if not os.path.exists(os.path.dirname(sublime_settings_path)):
-    os.makedirs(os.path.dirname(sublime_settings_path))
-
-  if not os.path.exists(os.path.dirname(sublime_user_path)):
-    os.makedirs(os.path.dirname(sublime_user_path))
-
-
-  # Set the default packages
-  if not os.path.isfile( sublime_user_path + 'Package Control.sublime-settings'):
-    with open( sublime_user_path + 'Package Control.sublime-settings', 'w+') as f:
-      print "Installing Default Packages"
-
-      best_packages = {
-        "installed_packages": [
-          "BracketHighlighter",
-          "Color Highlighter",
-          "ColorPicker",
-
-          "Emmet",
-          "JavaScript Completions",
-          "JavaScriptNext - ES6 Syntax",
-
-          "MarkdownEditing",
-          "Material Theme",
-          "Sass",
-          "SassBeautify",
-
-          "Package Control",
-          "SideBarEnhancements",
-          "SublimeCodeIntel",
-          "FileDiffs",
-          "Git",
-          "GitGutter",
-
-          "SublimeLinter",
-          "SublimeLinter-annotations",
-          "SublimeLinter-contrib-sass-lint",
-          "SublimeLinter-html-tidy",
-          "SublimeLinter-jshint",
-          "SublimeLinter-pep8",
-
-          "Web Inspector"
-        ]
-      }
-
-      prefs_plain = f.read()
-      prefs = {}
-
-      if prefs_plain != '':
-        prefs = json.loads(prefs_plain)
-
-      for key, value in best_packages.iteritems():
-        if key not in prefs:
-          prefs[key] = value
-
-      f.write(json.dumps(prefs, sort_keys=True, indent=4, separators=(',', ': ')))
-
-
-  # Set some default settings
-  if not os.path.isfile( sublime_user_path + 'Preferences.sublime-settings'):
-    with open( sublime_user_path + 'Preferences.sublime-settings', 'w+') as f:
-
-      print "Configuring Default Settings"
-
-      best_prefs = {
-        "always_show_minimap_viewport": False,
-        "bold_folder_labels": True,
-        #"theme": "Material-Theme.sublime-theme",
-        #"color_scheme": "Packages/Material Theme/schemes/Material-Theme.tmTheme",
-        "font_options": [
-          "gray_antialias",
-          "subpixel_antialias"
-        ],
-        "font_size": 16,
-        "indent_guide_options": [
-          "draw_normal",
-          "draw_active"
-        ],
-        "line_padding_bottom": 1,
-        "line_padding_top": 1,
-        "overlay_scroll_bars": "enabled",
-        "tab_size": 2,
-        "translate_tabs_to_spaces": True,
-        "trim_trailing_white_space_on_save": True,
-      }
-
-      prefs_plain = f.read()
-      prefs = {}
-
-      if prefs_plain != '':
-        prefs = json.loads(prefs_plain)
-
-      for key, value in best_prefs.iteritems():
-        if key not in prefs:
-          prefs[key] = value
-
-      f.write(json.dumps(prefs, sort_keys=True, indent=4, separators=(',', ': ')))
-
+print "Installing Developer Tools"
+os.system('brew cask install "%s"' % developer)
 
 # Oh-My-ZSH. Dracula Theme for iTerm2 needs to be installed manually
-if options['zsh'] == 'y':
-  print "Installing Oh-My-Zsh with Dracula Theme"
-  os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"')
-  os.system('brew install zsh-syntax-highlighting zsh-autosuggestions')
-  os.system('pip install pygments')
+print "Installing Oh-My-Zsh with Dracula Theme"
+os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"')
+os.system('brew install zsh-syntax-highlighting zsh-autosuggestions')
+os.system('pip install pygments')
 
-  if not os.path.isfile(os.path.expanduser("~") + '/.zshrc'):
-    os.system('cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc &> /dev/null')
+if not os.path.isfile(os.path.expanduser("~") + '/.zshrc'):
+os.system('cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc &> /dev/null')
 
-    # Agnoster Theme
-    os.system('sed -i -e \'s/robbyrussell/agnoster/g\' ~/.zshrc &> /dev/null')
-    # Plugins
-    os.system('sed -i -e \'s/plugins=(git)/plugins=(git git-flow brew sublime python node bower npm gem pip pod docker zsh-autosuggestions zsh-syntax-highlighting colorize colored-man-pages copydir copyfile extract)/g\' ~/.zshrc &> /dev/null')
+# Agnoster Theme
+os.system('sed -i -e \'s/robbyrussell/agnoster/g\' ~/.zshrc &> /dev/null')
+# Plugins
+os.system('sed -i -e \'s/plugins=(git)/plugins=(git git-flow brew sublime python node bower npm gem pip pod docker zsh-autosuggestions zsh-syntax-highlighting colorize colored-man-pages copydir copyfile extract)/g\' ~/.zshrc &> /dev/null')
 
-    # Customizations
-    os.system('echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc')
-    os.system('echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc')
-    os.system('echo "alias dog=\'colorize\'" >> ~/.zshrc')
+# Customizations
+os.system('echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc')
+os.system('echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc')
+os.system('echo "alias dog=\'colorize\'" >> ~/.zshrc')
 
-  os.system('chsh -s /bin/zsh &> /dev/null')
+os.system('chsh -s /bin/zsh &> /dev/null')
 
-  os.system('git clone https://github.com/zenorocha/dracula-theme/ ~/Desktop/dracula-theme/')
-
+os.system('git clone https://github.com/zenorocha/dracula-theme/ ~/Desktop/dracula-theme/')
 
 # Random OSX Settings
 print "Tweaking OSX Settings"
 
-if options['showhiddenfiles'] == 'y':
-  # Finder: show hidden files by default
-  os.system('defaults write com.apple.finder AppleShowAllFiles -bool true')
-  # Finder: show all filename extensions
-  os.system('defaults write NSGlobalDomain AppleShowAllExtensions -bool true')
-
+# Finder: show hidden files by default
+#os.system('defaults write com.apple.finder AppleShowAllFiles -bool true')
+# Finder: show all filename extensions
+os.system('defaults write NSGlobalDomain AppleShowAllExtensions -bool true')
 
 # Finder: allow text selection in Quick Look
 os.system('defaults write com.apple.finder QLEnableTextSelection -bool true')
 # Check for software updates daily
 os.system('defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1')
-# Disable auto-correct
-#os.system('defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false')
 # Require password immediately after sleep or screen saver begins
 os.system('defaults write com.apple.screensaver askForPassword -int 1')
 os.system('defaults write com.apple.screensaver askForPasswordDelay -int 0')
@@ -311,34 +242,17 @@ os.system('defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -boo
 os.system('sudo nvram SystemAudioVolume=", "')
 
 
-if options['animations'] == 'y':
-  print "Tweaking System Animations"
-  os.system('defaults write NSGlobalDomain NSWindowResizeTime -float 0.1')
-  os.system('defaults write com.apple.dock expose-animation-duration -float 0.15')
-  os.system('defaults write com.apple.dock autohide-time-modifier -float 0.2')
-  os.system('defaults write NSGlobalDomain com.apple.springing.delay -float 0.5')
-
-
-if options['autoupdate'] == 'y':
-  print "Enabling Automatic Brew Updates & Upgrades"
-  os.system('brew tap domt4/autoupdate')
-  os.system('brew autoupdate --start --upgrade')
-
+print "Tweaking System Animations"
+os.system('defaults write NSGlobalDomain NSWindowResizeTime -float 0.1')
+os.system('defaults write com.apple.dock expose-animation-duration -float 0.15')
+os.system('defaults write com.apple.dock autohide-time-modifier -float 0.2')
+os.system('defaults write NSGlobalDomain com.apple.springing.delay -float 0.5')
 
 # Make Google Chrome the default browser
 os.system('open -a "Google Chrome" --args --make-default-browser')
 
-
 # Clean Up
 os.system('brew cleanup && brew cask cleanup')
-
-
-print ""
-print ""
-print "*************************************"
-print "Enabling FileVault"
-os.system('sudo fdesetup enable')
-print ""
 
 print "*************************************"
 print "Your SSH Public Key Is:"
@@ -346,25 +260,16 @@ with open(os.path.expanduser("~") + '/.ssh/id_rsa.pub', 'r') as f:
   print f.read()
 print ""
 
-if options['zsh'] == 'y':
-  print "*************************************"
-  print "Remember to set up iTerm2:"
-  print "* Go to iTerm2 > Preferences > Profiles > Colors Tab"
-  print "  * Load Presets..."
-  print "  * Import..."
-  print "  * Pick Desktop > dracula-theme > iterm > Dracula.itermcolors"
-  print "* Go to iTerm2 > Preferences > Profiles > Text Tab"
-  print "  * Regular Font"
-  print "  * 12pt Menlo for Powerline Font"
-  print ""
-
-if options['sublime'] == 'y':
-
-  print "*************************************"
-  print "Please launch Sublime Text to finish setup"
-  print "Material Theme needs to be enabled manually"
-  print "On User Preferences, add: \"theme\": \"Material-Theme.sublime-theme\""
-  print ""
+print "*************************************"
+print "Remember to set up iTerm2:"
+print "* Go to iTerm2 > Preferences > Profiles > Colors Tab"
+print "  * Load Presets..."
+print "  * Import..."
+print "  * Pick Desktop > dracula-theme > iterm > Dracula.itermcolors"
+print "* Go to iTerm2 > Preferences > Profiles > Text Tab"
+print "  * Regular Font"
+print "  * 12pt Menlo for Powerline Font"
+print ""
 
 print "*************************************"
 print "Remember to restart your Mac"
